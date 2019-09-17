ce1d2021128ca54f23fe8d9a436d8b80aceecdc1
Jeriel Baptista Verissimo <jeriel.verissimo@protonmail.com>
2019-09-17 08:45:58 -0300

    #1 Removed scroll

--- lib/features/login/presentation/pages/login_screen.dart
+++ lib/features/login/presentation/pages/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:intolera/features/login/presentation/widgets/login_button_widget.dart';
 import 'package:intolera/features/login/presentation/widgets/sign_with_text_widget.dart';
 import 'package:intolera/features/login/presentation/widgets/social_button_row_widget.dart';
@@ -20,65 +19,62 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle.light,
-        child: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      Color(0xFFff80ab),
-                      Color(0xFFffac40),
-                    ],
-                    stops: [0.1, 0.9],
-                  ),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xFFff80ab),
+                    Color(0xFFffac40),
+                  ],
+                  stops: [0.1, 0.9],
                 ),
               ),
-              Container(
-                height: double.infinity,
-                child: SingleChildScrollView(
-                  physics: AlwaysScrollableScrollPhysics(),
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 40.0, vertical: 120.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        'Sign In',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'OpenSans',
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.bold,
-                        ),
+            ),
+            Container(
+              height: double.infinity,
+              child: SingleChildScrollView(
+                physics: AlwaysScrollableScrollPhysics(),
+                padding:
+                    EdgeInsets.symmetric(horizontal: 40.0, vertical: 120.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'OpenSans',
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
                       ),
-                      SizedBox(height: 30.0),
-                      email_input_text,
-                      SizedBox(height: 30.0),
-                      password_input,
-                      forgot_password_button_widget,
-                      buildRemeberMeCheckbox((value) {
-                        setState(() {
-                          _rememberMe = value;
-                        });
-                      }),
-                      login_button,
-                      sign_with_text,
-                      social_button_row,
-                      signup_button,
-                    ],
-                  ),
+                    ),
+                    SizedBox(height: 30.0),
+                    email_input_text,
+                    SizedBox(height: 30.0),
+                    password_input,
+                    forgot_password_button_widget,
+                    buildRemeberMeCheckbox((value) {
+                      setState(() {
+                        _rememberMe = value;
+                      });
+                    }),
+                    login_button,
+                    sign_with_text,
+                    social_button_row,
+                    signup_button,
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );

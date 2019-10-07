3395a75269b6829717a7cd42991664d5e92283c0
Jeriel Baptista Verissimo <jeriel.verissimo@protonmail.com>
2019-10-07 12:48:13 -0300

    #7 Impl exceptions interface

--- lib/features/core/usecases/usecase.dart
+++ lib/features/core/usecases/usecase.dart
@@ -1,4 +1,4 @@
-import 'package:intolera/core/error/failures.dart';
+import 'package:intolera/features/core/error/failures.dart';
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 

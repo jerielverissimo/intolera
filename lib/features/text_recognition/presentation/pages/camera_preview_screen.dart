3d8ac31e42fc1565d8ddaad4c291ce2f467f1720
Jeriel Baptista Verissimo <jeriel.verissimo@protonmail.com>
2019-11-07 16:23:18 -0300

    Trying to fix pipline

--- lib/features/text_recognition/presentation/pages/camera_preview_screen.dart
+++ lib/features/text_recognition/presentation/pages/camera_preview_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'dart:async';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
 class CameraPreviewScreen extends StatefulWidget {
   @override
@@ -10,13 +11,40 @@ class CameraPreviewScreen extends StatefulWidget {
 
 class _CameraPreviewScreen extends State<CameraPreviewScreen> {
   File _image;
+  FirebaseVisionImage vImage;
 
   Future getImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.camera);
 
     setState(() {
       _image = image;
+      vImage = FirebaseVisionImage.fromFile(_image);
+      readText();
     });
+  }
+
+  Future readText() async {
+    if (_image == null) {
+      print("Imagem nula");
+    }
+    TextRecognizer recognizeText = FirebaseVision.instance.textRecognizer();
+    VisionText visionText = await recognizeText.processImage(vImage);
+
+    String text = visionText.text;
+    for (TextBlock block in visionText.blocks) {
+      final Rect boundingBox = block.boundingBox;
+      final List<Offset> cornerPoints = block.cornerPoints;
+      final String text = block.text;
+      final List<RecognizedLanguage> languages = block.recognizedLanguages;
+
+      for (TextLine line in block.lines) {
+        // Same getters as TextBlock
+        for (TextElement element in line.elements) {
+          // Same getters as TextBlock
+          print(element.text);
+        }
+      }
+    }
   }
 
   @override
